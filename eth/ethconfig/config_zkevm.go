36cdac4bb7e19afaf0f02074913d7b85883cf3b6
lyh169 <liuyuhang203@sina.com>
2024-10-23 12:14:50 +0800

    add merlin testnet need replace block config update docker

--- eth/ethconfig/config_zkevm.go
+++ eth/ethconfig/config_zkevm.go
@@ -88,7 +88,8 @@ type Zk struct {
 	ExecutorPayloadOutput       string
 
 	TxPoolRejectSmartContractDeployments bool
-
+	// support for the block hash calculate
+	*Merlin
 	InitialBatchCfgFile            string
 	ACLPrintHistory                int
 	InfoTreeUpdateInterval         time.Duration
